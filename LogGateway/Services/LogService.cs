1e9ecbcb78383122fb4f79f9a542f329c4881633
kelvys B. Pantaleão <contato@kelvysb.com>
2021-10-30 16:29:29 -0300

    Quick adjust on template

--- LogGateway/Services/LogService.cs
+++ LogGateway/Services/LogService.cs
@@ -5,29 +5,29 @@ namespace LogGateway.Services
 {
     public class LogService : ILogService
     {
-        private const string template = "{message};{detail}";
+        private const string template = "{source};{message};{detail}";
         public void Debug(LogMessage message)
         {
             Getcontext(message.Source)
-                .Debug(template, message.Description, message.Detail);
+                .Debug(template, message.Source, message.Description, message.Detail);
         }
 
         public void Error(LogMessage message)
         {
             Getcontext(message.Source)
-                .Error(template, message.Description, message.Detail);
+                .Error(template, message.Source, message.Description, message.Detail);
         }
 
         public void Info(LogMessage message)
         {
             Getcontext(message.Source)
-                .Information(template, message.Description, message.Detail);
+                .Information(template, message.Source, message.Description, message.Detail);
         }
 
         public void Warning(LogMessage message)
         {
             Getcontext(message.Source)
-                .Warning(template, message.Description, message.Detail);
+                .Warning(template, message.Source, message.Description, message.Detail);
         }
 
         private static Serilog.ILogger Getcontext(string source)
